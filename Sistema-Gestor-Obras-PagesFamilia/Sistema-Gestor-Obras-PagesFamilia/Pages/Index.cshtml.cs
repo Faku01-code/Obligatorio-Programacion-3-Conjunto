838e5d0ddf6cbb68b52df23f5d9c8db2c61f18c7
Juan Meny <jemgprog@gmail.com>
2026-06-01 22:01:00 -0300

    Init

---
+++ Sistema-Gestor-Obras-PagesFamilia/Sistema-Gestor-Obras-PagesFamilia/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistema_Gestor_Obras_PagesFamilia.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
