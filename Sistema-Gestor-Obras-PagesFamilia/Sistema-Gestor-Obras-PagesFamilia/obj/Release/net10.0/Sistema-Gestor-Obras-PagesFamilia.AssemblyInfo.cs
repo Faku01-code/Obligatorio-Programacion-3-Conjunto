25e00dd5e4d2765b5de83551d5f7be363e235853
Faku01-code <facu01game@gmail.com>
2026-06-01 22:09:46 -0300

    Inicio

--- Sistema-Gestor-Obras-PagesFamilia/Sistema-Gestor-Obras-PagesFamilia/obj/Release/net10.0/Sistema-Gestor-Obras-PagesFamilia.AssemblyInfo.cs
+++ Sistema-Gestor-Obras-PagesFamilia/Sistema-Gestor-Obras-PagesFamilia/obj/Release/net10.0/Sistema-Gestor-Obras-PagesFamilia.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema-Gestor-Obras-PagesFamilia")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2b8dd4ce808de84f458c87bcd1570bdf8aab14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema-Gestor-Obras-PagesFamilia")]
